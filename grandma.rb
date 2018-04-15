a784efd82b40bc4738024bab833cace9a5c5fa53
Paulina Salem <paulinasalem@yahoo.com>
2018-04-15 01:14:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
-  if phrase.upcase
+if phrase="I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+elsif phrase.upcase
     "NO, NOT SINCE 1938!"
   elsif !phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase="I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
 end
 end
